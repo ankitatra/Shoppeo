d6cf92fc32d0949f1e5c6db68f77885b5edbbb42
ankitatra <ankitathakurankita7007gmail.com>
2023-02-15 22:27:25 +0530

    Add Navbar and Slider

--- FrontEnd/shoppeoapp/src/Pages/AllRoutes.jsx
+++ FrontEnd/shoppeoapp/src/Pages/AllRoutes.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route,Routes } from "react-router-dom";
 import { Navbar } from "../Components/Navbar";
+import Home from "./Home"
+import { Pay } from "./Payment";
 export const AllRoutes=()=>{
     return(
         <> 
         <Routes>
-            <Route path="/" element={<Navbar/>} />
+            <Route path="/" element={<Home/>} />
+            <Route path="/pay" element={<Pay/>}></Route>
+            <Route path="/success" element={<div>sucess</div>}></Route>
         </Routes>
        
         
---
+++ FrontEnd/shoppeoapp/src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Announcement from '../Components/Announcement'
+import Nnavbar from '../Components/Home_Navbar'
+import Slider from '../Components/Slider'
+
+const Home = () => {
+  return (
+    <div>
+      <Announcement/>
+      <Nnavbar/>
+      <Slider/>
+    </div>
+  )
+}
+
+export default Home
