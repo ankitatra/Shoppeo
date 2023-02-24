dfd4e2006a3d84461d3290a90152d412b73b4843
Ankita Thakur <ankitathakurankita7007@gmail.com>
2023-02-24 15:29:51 +0530

    add usertable

--- FrontEnd/shoppeoapp/src/Pages/Admin_page/userList/userList.jsx
+++ FrontEnd/shoppeoapp/src/Pages/Admin_page/userList/userList.jsx
@@ -1,75 +1,22 @@
 import "./userList.css";
-import { DataGrid } from "@material-ui/data-grid";
-// import { DeleteOutline } from "@material-ui/icons";
+
 import {MdDelete} from "react-icons/md"
 
 import { userRows } from "../../../Admin/dummyData/admin_dummy_data";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
-export default function UserList() {
-  //add userdta function
-  const [data, setData] = useState(userRows);
+import React from 'react'
 
-  const handleDelete = (id) => {
-    setData(data.filter((item) => item.id !== id));
-  };
-  
-  const columns = [
-    { field: "id", headerName: "ID", width: 90 },
-    {
-      field: "user",
-      headerName: "User",
-      width: 200,
-      renderCell: (params) => {
-        return (
-          <div className="userListUser">
-            <img className="userListImg" src={params.row.avatar} alt="" />
-            {params.row.username}
-          </div>
-        );
-      },
-    },
-    { field: "email", headerName: "Email", width: 200 },
-    {
-      field: "status",
-      headerName: "Status",
-      width: 120,
-    },
-    {
-      field: "transaction",
-      headerName: "Transaction Volume",
-      width: 160,
-    },
-    {
-      field: "action",
-      headerName: "Action",
-      width: 150,
-      renderCell: (params) => {
-        return (
-          <>
-            <Link to={"/user/" + params.row.id}>
-              <button className="userListEdit">Edit</button>
-            </Link>
-            <MdDelete
-              className="userListDelete"
-              onClick={() => handleDelete(params.row.id)}
-            />
-          </>
-        );
-      },
-    },
-  ];
-//add data of user
+const UserList = () => {
+  const [data,setdata]=useState(userRows)
   return (
-    <div className="userList">
-      <DataGrid
-        rows={data}
-        disableSelectionOnClick
-        columns={columns}
-        pageSize={8}
-        checkboxSelection
-      />
+    <div>
+     {data.map((item)=>{
+        return  <div>{item.id}</div>
+     })}
     </div>
-  );
+  )
 }
+
+export default UserList
